61e9600ef54e1ae9ac8a19404c2aa9fb67d2947b
Princess Irabor <irabor.princess1@gmail.com>
2019-06-26 03:45:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -26,7 +26,7 @@ def map_to_square(source_array)
   new_array
 end
 def reduce_to_total(source_array, starting_point*)
-  total = 0 
+  total = starting_point 
   source_array.length.times do |index|
     total += source_array[index]
   end
