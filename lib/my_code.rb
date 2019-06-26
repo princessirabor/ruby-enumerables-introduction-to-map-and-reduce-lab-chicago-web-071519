787c478909ddabd005c9ad47c139a734c392c55c
Princess Irabor <irabor.princess1@gmail.com>
2019-06-26 03:44:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
 # My Code here....
+def map_to_negativize(source_array)
+  new_array =[]
+  source_array.length.times do |index|
+    new_array[index] = source_array[index]*(-1)
+  end
+  new_array
+end
+
+def map_to_no_change(source_array)
+  source_array
+end
+
+def map_to_double(source_array)
+  new_array =[]
+  source_array.length.times do |index|
+    new_array[index] = source_array[index]*2 
+  end
+  new_array
+end
+def map_to_square(source_array)
+  new_array =[]
+  source_array.length.times do |index|
+    new_array[index] = source_array[index]**2 
+  end
+  new_array
+end
+def reduce_to_total(source_array)
+  total = 0 
+  source_array.length.times do |index|
+    total += source_array[index]
+  end
+  total
+end
+
